885ecacdd25e488ad46be8f86c16d58041c23983
hazzi125 <denchik.kiselevchik@gmail.com>
2018-09-20 23:15:52 +0700

    Started kurs

---
+++ kurs/kurs_saod.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <clocale>
+using namespace std;
+
+// сортировка по дате родения, ключ поиска - год рождения
+int N = 5;
+
+struct Man {
+	char name[30];
+	short int num;
+	char position[22];
+	char date[10];
+} Company[5];
+
+int main() {
+	//setlocale(LC_ALL, "rus");
+	FILE *f;
+	f = fopen("testBase2.dat", "r");
+	fread(&Company, sizeof(Company), N, f);
+	cout << "   |   Name/Surname/Patronymic    | Num |       Position        | Tel. number\n";
+	cout << "---|------------------------------|-----|-----------------------|-------------\n";
+	for(int i = 1; i <= N; i++) {
+		printf("%2d.| ", i);
+		cout << Company[i].name << "| ";
+		printf("%3d", Company[i].num);
+		cout << " | " << Company[i].position << " |   " << Company[i].date << "\n";
+	}
+	cout << "\n";
+	fclose(f);
+	system("pause");
+	return 0;
+}
