9ebf6a82cdfcc2f86841947e3f35f583f6ab3358
hazzi125 <denchik.kiselevchik@gmail.com>
2018-09-23 22:43:31 +0700

    Realisation of dynamic database

--- kurs/kurs_saod.cpp
+++ kurs/kurs_saod.cpp
@@ -2,32 +2,47 @@
 #include <cstdio>
 #include <cstdlib>
 #include <clocale>
+#include <string.h>
 using namespace std;
 
 // сортировка по дате родения, ключ поиска - год рождения
-const int N = 20;
-
+int N = 1000;
 
 struct Man {
 	char name[30];
 	short int num;
 	char position[22];
 	char date[10];
-} Company[N];
+};
 
 int main() {
 	//setlocale(LC_ALL, "rus");
+	Man *Company = new Man[N];
 	FILE *f;
-	f = fopen("testBase2.dat", "r");
-	fread(&Company, sizeof(Company), 1, f);
+	bool flag;
+	f = fopen("testBase2.dat", "rb");
+	fread(Company, sizeof(*Company), N, f);
+
 	cout << "   |   Name/Surname/Patronymic    | Num |       Position        | Tel. number\n";
 	cout << "---|------------------------------|-----|-----------------------|-------------\n";
-	for(int i = 0; i < N; i++) {
-		printf("%2d.| ", i+1);
-		cout << Company[i].name << "| ";
-		printf("%3d", Company[i].num);
-		cout << " | " << Company[i].position << " |   " << Company[i].date << "\n";
+	int k = 0;
+	
+	while(k < N) {
+		for(int i = 0; i < 20; i++) {
+			printf("%2d.| ", k+1);
+			cout << Company[k].name << "| ";
+			printf("%3d", Company[k].num);
+			cout << " | " << Company[k].position << " |   " << Company[k].date << "\n";
+			k++;
+	    }
+	    cout << "\nDo you want to print 20 more records? 1/0 ";
+		cin >> flag;
+		cout << "\n";
+		if(flag == 0) {
+			break;
+		}
 	}
+
 	cout << "\n";
 	fclose(f);
 	system("pause");
